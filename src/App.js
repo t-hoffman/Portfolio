877a63705338a5e02a056fc0c88443e8bdb54ea9
Tyler Hoffman <tjhoffmn@gmail.com>
2022-12-12 20:49:53 -0800

    Base portfolio complete

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
-export default function App() {
-  return <></>
+import './App.css';
+
+// IMPORT COMPONENTS
+import Header from './Components/Header.js';
+import Footer from './Components/Footer.js';
+import { Route, Routes } from 'react-router-dom';
+// IMPORT PAGES
+import Home from 'Pages/Home';
+import About from 'Pages/About';
+import Projects from 'Pages/Projects';
+
+export default function App () {
+  const URL = '';
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
---
+++ src/Pages/Projects.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+
+const Projects = (props) => {
+    const [projects, setProjects] = useState(null);
+
+    const getProjectData = async () => {
+        const response = await fetch('./projects.json');
+        const data = await response.json();
+        setProjects(data);
+    };
+
+    useEffect(() => {
+        getProjectData()
+    }, []);
+console.log(projects)
+    const loaded = () => {
+        return projects.map((project) => (
+            <div>
+                <h1>{project.name}</h1>
+                {
+                    project.image.map((img) => {
+                        return <img src={img} height="450px" />
+                    })
+                }
+                <p>
+                    <a href={project.git}>
+                    <button>Github</button>
+                    </a>
+                {
+                    project.live.map((live) => {
+                        return (
+                            <a href={live}>
+                            <button>Live Site</button>
+                            </a>
+                        )
+                    })
+                }
+                </p>
+            </div>
+        ));
+    };    
+
+    return projects ? loaded() : <h1>Loading...</h1>;
+}
+
+export default Projects;
